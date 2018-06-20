d621e1f7adc496ba483f941d1b19398efe302260
srappaport <sarah.rappaport@greendot.org>
2018-06-20 15:51:01 +0000

    automatically backed up by learn

---
+++ amazon.rb
@@ -0,0 +1,7 @@
+## This code adds sales tax to the price of the product
+
+abq_tax = .05125
+
+## First, we will build our price list
+cart_contains = ["toothbrush", "jacket", ""]
+cart_prices = [1.50, 42.99, 
