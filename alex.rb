85eee8c90d7d4c6be21f2bf0dec7bc1d9f968d57
srappaport <sarah.rappaport@greendot.org>
2018-06-20 15:47:15 +0000

    automatically backed up by learn

---
+++ alex.rb
@@ -0,0 +1,5 @@
+items = {}
+items["24K magic CD"] =15.98
+items["crocs"] = 35
+puts items
+puts items["crocs"]
---
+++ fizzbuzz.rb
@@ -0,0 +1,19 @@
+
+def fizzbuzz(number)
+
+
+if number%15 == 0
+  puts "FizzBuzz"
+  "FizzBuzz"
+elsif number%5 == 0
+  puts "Buzz"
+  "Buzz"
+elsif number%3 == 0
+  puts "Fizz"
+  "Fizz"
+else
+end
+
+
+end
+fizzbuzz(14)
